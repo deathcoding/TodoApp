fc364b421de90e8102eaa59cae6132430328d704
Ilgizik <ilishev.i@yandex.ru>
2025-02-27 14:46:48 +0500

    build in folder dist ready 2

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 
+console.log("fdvdfv");
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
